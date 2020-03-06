6790ae51438050d8ee877ada5859a469e03d0a75
eddyguan-cn <61837077+eddyguan-cn@users.noreply.github.com>
2020-03-06 15:31:58 +0800

    99乘法表

---
+++ b.py
@@ -0,0 +1,16 @@
+for a in range(1,10):
+    for b in range(1,a+1):
+        c=a*b
+        if b<=a:
+            print('{}*{}'.format(b,a),c)
+            if b>a:
+                break
+
+
+
+
+
+
+
+
+
